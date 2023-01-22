4b2d9ae299b0cb1e69511e3a0a66795c7af38072
Mohd Naveed Khan <Naveed.khan@mahiratechnology.com>
2023-01-22 13:01:12 +0530

    scripts

---
+++ ALB/main.tf
@@ -0,0 +1,19 @@
+
+
+resource "aws_lb" "alb" {
+  name               = "naveedalb" 
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [var.security_groups]
+  subnets = [
+    var.subnet1,
+    var.subnet2
+  ]
+
+
+  enable_deletion_protection = false
+  
+  tags = {
+    Environment = "production"  // key value pair
+  }
+}
